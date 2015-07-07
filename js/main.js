320f42451530900c4a6bdd6a7cf23f58ce565ef7
SashaSansay <mc_nine@bk.ru>
2015-07-07 20:43:46 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     function init() {
         var mapOptions = {
             center: new google.maps.LatLng(53.2225657,50.1905889),
-            zoom: 12,
+            zoom: 13,
             zoomControl: false,
             disableDoubleClickZoom: true,
             mapTypeControl: false,
@@ -23,24 +23,28 @@ $(document).ready(function(){
         var locations = [
             ['WiFiX', 'undefined', 'undefined', 'undefined', 'undefined', 53.2225657, 50.1905889, 'undefined']
         ];
-        for (i = 0; i < locations.length; i++) {
-            if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-            if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-            if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-            if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-            if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
-            marker = new google.maps.Marker({
-                icon: markericon,
-                position: new google.maps.LatLng(locations[i][5], locations[i][6]),
-                map: map,
-                title: locations[i][0],
-                desc: description,
-                tel: telephone,
-                email: email,
-                web: web
-            });
-            link = '';     }
-
+        new google.maps.Marker({
+            icon: "https://raw.githubusercontent.com/SashaSansay/wifix/master/img/pin.png",
+            position: new google.maps.LatLng(53.2225657, 50.1905889),
+        })
+        //for (i = 0; i < locations.length; i++) {
+        //    if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
+        //    if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
+        //    if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
+        //    if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
+        //    if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
+        //    marker = new google.maps.Marker({
+        //        icon: markericon,
+        //        position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+        //        map: map,
+        //        title: locations[i][0],
+        //        desc: description,
+        //        tel: telephone,
+        //        email: email,
+        //        web: web
+        //    });
+        //    link = '';     }
+//
     }
     init();
 });
